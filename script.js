f96c80cffe139976fd2873a4eaa2eaa069bb3a68
Fabiana Santos Soares <faabiana.macedo@gmail.com>
2023-03-14 22:24:58 +0044

    Validação de formulário pelo JS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* JS para carrosel de imagens */
+
 const imgs = document.getElementById("img-carrosel")
 const img = document.querySelectorAll("#img-carrosel img")
 
@@ -14,3 +16,42 @@ imgs.style.transform = `translateX(${-i * 500}px)`;
 }
 
 setInterval (carrosel, 3000);
+
+/* JS para validação do formulário */
+
+const form  = document.getElementById("formulario");
+const nome  = document.getElementById("user-name");
+const email = document.getElementById("user-email");
+const msg   = document.getElementById("msg-user");
+const span  = document.querySelectorAll(".span-required");
+const emailRegex = /\S+@\S+\.\S+/;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    nameValidate();
+    emailValidate();
+
+});
+
+function nameValidate (){
+    if (nome.value.length < 3){
+        nome.style.border = '2px solid #FF0000';
+        span[0].style.display = 'block'
+    } else {
+        nome.style.border = ''
+        span[0].style.display = 'none'
+     }
+    }
+
+function emailValidate (){
+    if (!emailRegex.test(email.value)){
+        //console.log('Email inválido!')
+        email.style.border = '2px solid #FF0000';
+        span[1].style.display = 'block'
+    } else {
+        //console.log('Email válido')
+        email.style.border = ''
+        span[1].style.display = 'none'
+     }
+    }
+
